8b573a18ccd034fcde38a7a131cd37e9a7f6b980
faridpark12 <faridpark12@gmail.com>
2019-10-22 18:19:36 +0900

    그래들 업그레이드 @Matt 응암

--- app/src/main/java/mms5/onepagebook/com/onlyonesms/MainActivity.java
+++ app/src/main/java/mms5/onepagebook/com/onlyonesms/MainActivity.java
@@ -44,7 +44,7 @@ import mms5.onepagebook.com.onlyonesms.model.Statistics;
 import mms5.onepagebook.com.onlyonesms.util.Utils;
 
 public class MainActivity extends AppCompatActivity implements Constants {
-    public static final boolean HAS_TO_SHOW_LOGS = false;
+    public static final boolean HAS_TO_SHOW_LOGS = true;
     private static final int REQUEST_DEFAULT_APP = 12;
     private static final int REQUEST_PERMISSION = 120;
 
