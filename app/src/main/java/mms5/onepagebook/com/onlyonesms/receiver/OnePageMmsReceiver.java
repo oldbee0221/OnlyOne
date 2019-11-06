7daa41114d838853b6347c55cbab21503b2a4c0a
Park Jeong Ho <faridpark12@gmail.com>
2019-11-06 20:14:57 +0900

    수정
    mms 수신 시 mms 처리 못하는 거 수정, 콜백 기능 활성화

--- app/src/main/java/mms5/onepagebook/com/onlyonesms/receiver/OnePageMmsReceiver.java
+++ app/src/main/java/mms5/onepagebook/com/onlyonesms/receiver/OnePageMmsReceiver.java
@@ -1,9 +1,5 @@
 package mms5.onepagebook.com.onlyonesms.receiver;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,39 +8,28 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.provider.Telephony;
-import android.support.v4.app.NotificationCompat;
-import android.text.TextUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.android.mms.transaction.PushReceiver;
+
 import java.text.MessageFormat;
 
-import me.leolin.shortcutbadger.ShortcutBadger;
 import mms5.onepagebook.com.onlyonesms.CBMListActvitity;
-import mms5.onepagebook.com.onlyonesms.R;
-import mms5.onepagebook.com.onlyonesms.SteppingStoneActivity;
-import mms5.onepagebook.com.onlyonesms.api.ApiCallback;
-import mms5.onepagebook.com.onlyonesms.api.Client;
-import mms5.onepagebook.com.onlyonesms.api.body.SendingChangedNumberBody;
-import mms5.onepagebook.com.onlyonesms.api.response.DefaultResult;
 import mms5.onepagebook.com.onlyonesms.common.Constants;
-import mms5.onepagebook.com.onlyonesms.manager.GsonManager;
-import mms5.onepagebook.com.onlyonesms.manager.PreferenceManager;
-import mms5.onepagebook.com.onlyonesms.manager.RetrofitManager;
-import mms5.onepagebook.com.onlyonesms.model.UserInfo;
 import mms5.onepagebook.com.onlyonesms.util.Utils;
 
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
-public class OnePageMmsReceiver extends BroadcastReceiver implements Constants {
+/**
+ * Created by jeonghopark on 2019-11-06.
+ */
+public class OnePageMmsReceiver extends PushReceiver implements Constants {
     private static long mRecvMillis = 0L;
     private Context _context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Utils.Log("OnePageMmsReceiver");
+        Utils.Log("OnePageMmsReceiver ======");
+        super.onReceive(context, intent);
 
         _context = context;
 
@@ -56,67 +41,6 @@ public class OnePageMmsReceiver extends BroadcastReceiver implements Constants {
         };
         Handler handler = new Handler();
         handler.postDelayed(runn, 6000);
-    }
-
-    private void uploadSms(Context context, String address, String message) {
-        if (!TextUtils.isEmpty(PreferenceManager.getInstance(context).getUseJson())) {
-            UserInfo userInfo = GsonManager.getGson().fromJson(PreferenceManager.getInstance(context).getUseJson(), UserInfo.class);
-
-            if (!TextUtils.isEmpty(userInfo.id)) {
-                String number = Utils.getPhoneNumber(context);
-                RetrofitManager.retrofit(context).create(Client.class)
-                        .sendChangedNumber(new SendingChangedNumberBody(userInfo.id, number, address, message))
-                        .enqueue(new ApiCallback<DefaultResult>() {
-                            @Override
-                            public void onSuccess(DefaultResult response) {
-                            }
-
-                            @Override
-                            public void onFail(int error, String msg) {
-                            }
-                        });
-            }
-        }
-    }
-
-    private boolean checkMsg(String m) {
-        if (m.contains(FRAG_01)) {
-            if (m.contains(FRAG_S4)) return true;
-            if (m.contains(FRAG_S8)) return true;
-            if (m.contains(FRAG_S6)) return true;
-            if (m.contains(FRAG_S7)) return true;
-            if (m.contains(FRAG_S2)) return true;
-            if (m.contains(FRAG_S1)) return true;
-            if (m.contains(FRAG_S3)) return true;
-            if (m.contains(FRAG_S5)) return true;
-        }
-
-        return false;
-    }
-
-    private void spitNotification(Context context, String message) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle(context.getString(R.string.app_name));
-        builder.setContentText(message);
-        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);
-        builder.setAutoCancel(true);
-
-        Intent intent = new Intent(context, SteppingStoneActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.setContentIntent(contentIntent);
-
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
-        }
-
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
-
-        int badgeCount = Utils.GetIntSharedPreference(context, PREF_BADGE_CNT) + 1;
-        ShortcutBadger.applyCount(context, badgeCount);
-        Utils.PutSharedPreference(context, PREF_BADGE_CNT, badgeCount);
     }
 
     private void parseMMS() {
@@ -135,8 +59,7 @@ public class OnePageMmsReceiver extends BroadcastReceiver implements Constants {
         cursor.close();
 
         String number = parseNumber(id);
-        String msg = parseMessage(id);
-        Utils.Log("MMSReceiver.java | parseMMS |" + number + "|" + msg);
+        Utils.Log("MMSReceiver.java | parseMMS |" + number + "|");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
                 && Telephony.Sms.getDefaultSmsPackage(_context).equals(_context.getPackageName())) {
@@ -182,69 +105,5 @@ public class OnePageMmsReceiver extends BroadcastReceiver implements Constants {
         cursor.close();
 
         return result;
-    }
-
-    private String parseMessage(String $id) {
-        String result = null;
-
-        // 조회에 조건을 넣게되면 가장 마지막 한두개의 mms를 가져오지 않는다.
-        Cursor cursor = _context.getContentResolver().query(Uri.parse("content://mms/part"), new String[] { "mid", "_id", "ct", "_data", "text" }, null, null, null);
-
-        Utils.Log("MMSReceiver.java | parseMessage |mms 메시지 갯수 : " + cursor.getCount() + "|");
-        if (cursor.getCount() == 0) {
-            cursor.close();
-            return result;
-        }
-
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            String mid = cursor.getString(cursor.getColumnIndex("mid"));
-            if ($id.equals(mid)) {
-                String partId = cursor.getString(cursor.getColumnIndex("_id"));
-                String type = cursor.getString(cursor.getColumnIndex("ct"));
-                if ("text/plain".equals(type)) {
-                    String data = cursor.getString(cursor.getColumnIndex("_data"));
-
-                    if (TextUtils.isEmpty(data))
-                        result = cursor.getString(cursor.getColumnIndex("text"));
-                    else
-                        result = parseMessageWithPartId(partId);
-                }
-            }
-            cursor.moveToNext();
-        }
-        cursor.close();
-
-        return result;
-    }
-
-
-    private String parseMessageWithPartId(String $id) {
-        Uri partURI = Uri.parse("content://mms/part/" + $id);
-        InputStream is = null;
-        StringBuilder sb = new StringBuilder();
-        try {
-            is = _context.getContentResolver().openInputStream(partURI);
-            if (is != null) {
-                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-                BufferedReader reader = new BufferedReader(isr);
-                String temp = reader.readLine();
-                while (!TextUtils.isEmpty(temp)) {
-                    sb.append(temp);
-                    temp = reader.readLine();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-
-        return sb.toString();
     }
 }
